b17e652d503425f4b7bef223aca14b119662ab03
Ripsime <hripsimesargsyan90575@gmail.com>
2026-01-13 20:22:58 +0400

    canginnn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Aviasales_Prototype/', 
   plugins: [react()],
-  base: "/Aviasales_Prototype/",
 })
