d0a9b7b02af4dff567fc4862d1897b275550e92c
Ripsime <hripsimesargsyan90575@gmail.com>
2026-01-13 20:19:11 +0400

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Aviasales_Prototype/",
+  base: '/Aviasales_Prototype/',
+  plugins: [react()]
 })
